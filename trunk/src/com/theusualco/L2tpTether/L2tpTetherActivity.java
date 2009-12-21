4a441f6c58b6f2e8c13415977ec116c488993223
iain.wade <iain.wade@f1de4cce-edd2-11de-a198-37193048d1ed>
2009-12-21 02:50:01 +0000

    Add project skeleton.

---
+++ trunk/src/com/theusualco/L2tpTether/L2tpTetherActivity.java
@@ -0,0 +1,15 @@
+package com.theusualco.L2tpTether;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class L2tpTetherActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
